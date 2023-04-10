62fd0c010e10802d19f1c0869a8169fdb73c63f2
James West <dwest62@gmail.com>
2023-04-10 19:41:20 +0000

    debug

--- week4/edit.php
+++ week4/edit.php
@@ -13,18 +13,32 @@
 		Date: 4/10/23
 	-->
 </head>
-
 <body>
     <h1>CSC 235 Group Project Admin</h1>
-    <button onClick="deploy()">Deploy Changes to Server</button>
+    <form action="javascript:deploy()">
+        <label for="msg">Update message:</label>
+        <input type="text" id="msg"><br />
+        <button type="submit">Deploy Changes to Server</button>
+    </form>
+    <p id="notify"></p>
     <p id="results"></p>
 </body>
-
 <script>
     async function deploy() {
-        const response = await fetch("/serverSideDev/week4/updateServer.php");
-        document.getElementById("results").innerHTML = response.text();
+        const msgVal = document.getElementById('msg').value;
+        const msg = "Deploying to production server using message: " + (msgVal ? msgVal : "deploy to production");        
+        document.getElementById('notify').innerHTML = msg;
+
+        const response = await fetch("/serverSideDev/week4/updateServer.php", {
+            method: 'POST',
+            body: JSON.stringify({
+                msg: msg
+            }),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        });
         console.log(response);
+        const text = await response.text();
+        document.getElementById("results").innerHTML = text;
     }
 </script>
 
--- week4/updateServer.php
+++ week4/updateServer.php
@@ -7,29 +7,44 @@
      *  Date: 4/10/23
     */
 
-	// Change directory to git directory
-	chdir('../groupProject');
-	
-	// Checkout master branch
-	runShellCommand('Switching to master', 'git checkout master 2>&1');
+	$postData = file_get_contents("php://input");
+	echo "t";
 
-	// Pull updates from remote source (github)
-	runShellCommand('Updating local master branch', 'git pull 2>&1');
+	// if($postData == false) 
+	// {
+	// 	echo "There was an error with this request";
+	// } 
+	// else 
+	// {
+	// 	$postData = json_decode($postData);
+	// 	deploy($postData['msg']);
+	// }
 
-	// Switch back to prod branch
-	runShellCommand('Switching back to prod branch', 'git checkout prod 2>&1');
+	// function deploy($msg)
+	// {
+	// 	chdir('../groupProject');
+	// 	$result = "";
+		
+	// 	// Checkout master branch
+	// 	$result .= runShellCommand('Switching to master', 'git checkout master 2>&1');
 
-	// Merge change updates from master to prod
-	$date = date();
-	runShellCommand('Merging changes from master to prod', 'git merge master -m' . '"Push to prod -' . $date . '"');
+	// 	// Pull updates from remote source (github)
+	// 	$result .= runShellCommand('Updating local master branch', 'git pull 2>&1');
 
-	// Push updates to remote (github)
-		runShellCommand('Updating remote prod branch', 'git push 2>&1');
+	// 	// Switch back to prod branch
+	// 	$result .= runShellCommand('Switching back to prod branch', 'git checkout prod 2>&1');
 
-	function runShellCommand($msg, $commandString)
-	{
-		echo "<p>" . $msg . "<br/>";
-		echo "<pre>" . shell_exec($commandString) . "</pre>";
-		echo "</p>";
-	}
+	// 	// Merge change updates from master to prod
+	// 	$result .= runShellCommand('Merging changes from master to prod', 'git merge master -m Merged');
+
+	// 	// Push updates to remote (github)
+	// 	$result .= runShellCommand('Updating remote prod branch', 'git push 2>&1');
+
+	// 	function runShellCommand($msg, $commandString)
+	// 	{
+	// 		return "<p>" . $msg . "<br/><pre>" . shell_exec($commandString) . "</pre></p>";
+	// 	}
+
+	// 	echo $result;
+	// }
 ?>
