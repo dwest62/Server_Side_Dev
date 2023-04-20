d160d73b2e030a8ee39d85eb5f5a05074dc723ec
jwest <jwest@csp.edu>
2023-04-20 16:25:55 -0500

    added tag type class

---
+++ groupProject/db/component/TagType.php
@@ -0,0 +1,50 @@
+<?php
+
+class TagType
+{
+    private int $id;
+    private string $name;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param int|null $id
+     * @param string|null $name
+     */
+    public function __construct(?int $id = 0, ?string $name = "")
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+}
